702ebae64faec9f21eb47bb28e42b3b557504be3
JavaInTheMorning <34899870+JavaInTheMorning@users.noreply.github.com>
2018-03-25 19:19:54 -0400

    Fixed Ilab errors

--- myallocate.c
+++ myallocate.c
@@ -2,43 +2,32 @@
 // Created by dholl_000 on 3/24/2018.
 //
 
-#define malloc(x) myallocate(x, __FILE__, __LINE__, THREADREQ)
-#define free(x) mydeallocate(x, __FILE__, __LINE__, THREADREQ)
 
-#define BLOCK_SIZE 8000000;
 
 #include "myallocate.h"
 #include<stddef.h>
 #include<stdio.h>
 
-char memory[BLOCK_SIZE];
-struct block *blocklist;
-
-int main {
-    blocklist = (void *) memory;
-}
 
 void initialize(struct block *ptr) {
-    blocklist->size = BLOCK_SIZE
-    -sizeof(struct block); //avaialbbe space for malloc
+    blocklist->size = BLOCK_SIZE -sizeof(block); //avaialbbe space for malloc
     blocklist->free = '1'; // initalize the block to signal it is free
     blocklist->next = NULL;
 }
 
-void
-split(struct block *fill, size_t size) {//use to split memory for a requested sized that is smaller than what we have
-    struct block *partiation = (void *) ((void *) fill + size + sizeof(struct block));
-    partiation->size = (fill->size) - size - sizeof(struct block);
-    partiation->free = '1';
-    partiation->next = fill->next;
+void split(block *fill, size_t size) {//use to split memory for a requested sized that is smaller than what we have
+    block *partition = (void *) ((void *) fill + size + sizeof(block));
+    partition->size = (fill->size) - size - sizeof(block);
+    partition->free = '1';
+    partition->next = fill->next;
     fill->size = size;
     fill->free = '0';
-    fill->next = partiation->next;
+    fill->next = partition->next;
 
 }
 
-void* myMalloc(size_t sizeBits, RequestType) {
-    struct block *current, *prev; //the current and previous memory blocks
+void* myAllocate(size_t sizeBits) {
+    block *current, *prev; //the current and previous memory blocks
 
     if (blocklist->size >= BLOCK_SIZE){
         printf("Error, not initialized");
@@ -52,37 +41,37 @@ void* myMalloc(size_t sizeBits, RequestType) {
     current = blocklist; //set pointer to first block;
 
     //loop through to find the frist free block to use for allocation that is big enough
-    while (((current->size < sizeBits) || (current->free == 0)) && (current.next != NULL)) {
+    while (((current->size < sizeBits) || (current->free == 0)) && (current->next != NULL)) {
         prev = current;
-        current = current.next;
+        current = current->next;
     }//will loop until find a block that can be allocated
 
     //case 1, requested size fits
     if (sizeBits >= 8) { //for large allocations
-        current->position = prev->pos - sizeBits;                            /**********|*****|*****/
-        current.free = '0';
-        current.next = NULL;
-        return (void *)(current.position);
+        current->pos = prev->pos - sizeBits;                            /**********|*****|*****/
+        current->free = '0';
+        current->next = NULL;
+        return (void *)(current->pos);
 
 
     } else { //for small allocations
-        current->position = prev->pos + prev->size; /*|******/
+        current->pos = prev->pos + prev->size; /*|******/
         current->free = '0' ;
-        current.next = NULL;
+        current->next = NULL;
         prev->next = current;//set prev next to current block that was filled
-        return (void *)(current.position);
+        return (void *)(current->pos);
     }
 
 }
 
 void merge() { //to help elimatante space, merge consective free blocks of data
-    struct block *current, *prev;
+    block *current, *prev;
     current = blocklist;
 
-    while ((current && current.next) {
+    while (current && current->next) {
 
         if ((current->free) && (current->next->free)) {
-            current.size += (current->next->size + sizeof(struct block));
+            current->size += (current->next->size + sizeof(block));
             current->next = current->next->next;
         }
 
@@ -93,10 +82,10 @@ void merge() { //to help elimatante space, merge consective free blocks of data
 
 }
 
-void mydeallocate(void *ptr) {
+void myDeAllocate(void *ptr) {
     if (((void *) memory <= ptr) && (ptr <= (void *) memory + BLOCK_SIZE)){
 
-        struct block *current = ptr;
+        block *current = ptr;
         current = current - sizeof(current);
         current->free = 1;
         merge();
--- myallocate.h
+++ myallocate.h
@@ -11,12 +11,12 @@
 #include<stddef.h>
 #include<stdio.h>
 
-#define malloc(x) myallocate(x, __FILE__, __LINE__, THREADREQ)
-#define free(x) mydeallocate(x, __FILE__, __LINE__, THREADREQ)
+#define THREADREQ 1
 
-void initialize();
-void split(struct block *fiting_slot, size_t size) ;
-void myallocate(size_t sizeBits, __FILE__, __LINE__, THREADREQ);
+#define malloc(x) myAllocate(x)
+#define free(x) myDeAllocate(x)
+
+#define BLOCK_SIZE 8000000
 
 //metadata block,
 typedef struct block{
@@ -26,15 +26,19 @@ typedef struct block{
     //char data[1]; //used for splitting blocks?
     struct block *next; //pointer to next block's metadata
 
-};
-typedef struct page{
-    sig_atomic_t count ; //number of references to page
-    void *virtual; //kernel virtual address of page, if it mapped into kernel, null otherwise
-    unsigned long flags ; //status of page, used for read/write procetions 
+}block;
 
-};
 
 typedef enum {
     TYPE_USER, TYPE_THREAD
 }RequestType;
+
+char memory[BLOCK_SIZE];
+block *blocklist = (void*) memory;
+
+void initialize();
+void split(struct block *fitting_slot, size_t size);
+void* myAllocate(size_t sizeBits);
+void myDeAllocate(void *ptr);
+
 #endif //ASST1_MYALLOCATE_H
